a1a7579eef007ce389c8c88f2214d14cfeb12aaf
sergey <andreevsv85@gmail.com>
2024-07-08 11:32:33 +0300

    ALL

---
+++ LAPTOP/Тест/instrok.py
@@ -0,0 +1,6 @@
+expale = 'топинамбур'
+print(expale[0])
+print(expale[9])
+print(expale[6:10])
+print(expale[::-1])
+print(expale[1::2])
---
+++ LAPTOP/Тест/test.py
@@ -0,0 +1,10 @@
+salary = [300, 400.50, 800, 456.10]
+SEMIYA = ["Dima", 'Roma', 'Eva', 'Serg']
+# print(sum(salary) / len(salary))  # Средняя зарплата
+# print(max(salary),"МАКСИМАЛКА")
+Slovar = dict(zip(SEMIYA, salary))
+print(Slovar['Dima'], Slovar['Roma'], 'зарплата  Димы и РОмы')
+
+
+
+
